eb2a686e130e6ac78261d8448b3891dcc932169f
ItzMiracleOwO <s15029@kings.edu.hk>
2023-05-08 19:32:15 +0800

    Linter fix Ep. 2

--- commands/mdesk/docker.js
+++ commands/mdesk/docker.js
@@ -2,79 +2,79 @@ const {
   ActionRowBuilder,
   StringSelectMenuBuilder,
   SlashCommandBuilder,
-  EmbedBuilder,
-} = require("discord.js");
-const config = require("../../config.json");
+  EmbedBuilder
+} = require('discord.js')
+const config = require('../../config.json')
 
-const Docker = require("dockerode");
-const docker = new Docker({ socketPath: config.dockerSock });
+const Docker = require('dockerode')
+const docker = new Docker({ socketPath: config.dockerSock })
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("docker")
-    .setDescription("Docker management"),
-  async execute(interaction) {
-    const containerList = [];
+    .setName('docker')
+    .setDescription('Docker management'),
+  async execute (interaction) {
+    const containerList = []
     docker.listContainers(async function (err, containers) {
       if (err) {
-        console.log(err);
-        interaction.reply("An error occured!");
+        console.log(err)
+        interaction.reply('An error occured!')
       } else {
         containers.map(async function (containerInfo) {
           // console.log(containerInfo.Id, containerInfo.Names)
 
           let containers =
-            "Hash: " +
+            'Hash: ' +
             containerInfo.Id.substring(0, 12) +
-            " Name: " +
-            containerInfo.Names;
-          containers = containers.replace("/", "");
+            ' Name: ' +
+            containerInfo.Names
+          containers = containers.replace('/', '')
           //    containers = `"` + containers + `"`;
 
-          containerList.push(containers);
+          containerList.push(containers)
           // console.log(containers);
-        });
+        })
 
         // console.log(containerList);
         let options = containerList.map((item) => {
-          const [hash, name] = item.split(" Name: ");
-          const hashValue = hash.replace("Hash: ", "");
-          const nameValue = name.trim();
+          const [hash, name] = item.split(' Name: ')
+          const hashValue = hash.replace('Hash: ', '')
+          const nameValue = name.trim()
           const optionData = {
             emoji: undefined,
             label: nameValue,
             description: hashValue,
-            value: hashValue,
-          };
-          return optionData;
-        });
+            value: hashValue
+          }
+          return optionData
+        })
 
-        options = options.slice(0, 23);
+        options = options.slice(0, 23)
 
         // console.log(options);
 
         const select = new StringSelectMenuBuilder()
-          .setCustomId("containerSelection")
-          .setPlaceholder("Select container")
-          .addOptions(options);
+          .setCustomId('containerSelection')
+          .setPlaceholder('Select container')
+          .addOptions(options)
 
         const dockerEmbed = new EmbedBuilder()
           .setColor(0x0099ff)
-          .setTitle("Docker")
-          .setDescription("Select a conainer and input")
+          .setTitle('Docker')
+          .setDescription('Select a conainer and input')
           .setTimestamp()
           .setFooter({
-            text: "mDesk by mTech",
-          });
+            text: 'mDesk by mTech'
+          })
         // console.log(containers);
-        const row = new ActionRowBuilder().addComponents(select);
+        const row = new ActionRowBuilder().addComponents(select)
 
         interaction.reply({
           embeds: [dockerEmbed],
           components: [row],
-          fetchReply: true,
-        });
+          fetchReply: true
+        })
       }
-    });
-  },
-};
+    })
+  }
+}
--- events/dockerSelect.js
+++ events/dockerSelect.js
@@ -1,58 +1,58 @@
 // Docker command handler
-const { Events, EmbedBuilder } = require("discord.js");
-const config = require("../config.json");
+const { Events, EmbedBuilder } = require('discord.js')
+const config = require('../config.json')
 
-const Docker = require("dockerode");
-const docker = new Docker({ socketPath: config.dockerSock });
+const Docker = require('dockerode')
+const docker = new Docker({ socketPath: config.dockerSock })
 
-const si = require("systeminformation");
+const si = require('systeminformation')
 
 module.exports = {
   name: Events.InteractionCreate,
-  async execute(interaction) {
-    const dockerArray = await si.dockerAll();
-    if (!interaction.isStringSelectMenu()) return;
-    if (interaction.customId !== "containerSelection") return;
+  async execute (interaction) {
+    const dockerArray = await si.dockerAll()
+    if (!interaction.isStringSelectMenu()) return
+    if (interaction.customId !== 'containerSelection') return
 
     await interaction.deferReply({
-      message: "Please wait...",
-      fetchReply: true,
-    });
-    const container = interaction.values[0];
+      message: 'Please wait...',
+      fetchReply: true
+    })
+    const container = interaction.values[0]
 
     const dockerResult = dockerArray.find((element) =>
       element.id.startsWith(container)
-    );
+    )
 
-    const containerHash = dockerResult.id.substring(0, 12);
-    const containerName = dockerResult.name;
-    const containerImage = dockerResult.image;
-    const containerCommand = dockerResult.command;
-    const containerStatus = dockerResult.state;
+    const containerHash = dockerResult.id.substring(0, 12)
+    const containerName = dockerResult.name
+    const containerImage = dockerResult.image
+    const containerCommand = dockerResult.command
+    const containerStatus = dockerResult.state
 
     const dockerStatuseEmbed = new EmbedBuilder()
-      .setTitle("Docker container status")
+      .setTitle('Docker container status')
       .setDescription(
         `Container Name: ${containerName}` +
-          "\n" +
+          '\n' +
           `Container Hash: ${containerHash}`
       )
       .addFields(
-        { name: "Container image", value: `${containerImage}`, inline: true },
-        { name: "\u200B", value: "" },
+        { name: 'Container image', value: `${containerImage}`, inline: true },
+        { name: '\u200B', value: '' },
         {
-          name: "Container command",
+          name: 'Container command',
           value: `${containerCommand}`,
-          inline: true,
+          inline: true
         },
-        { name: "\u200B", value: "" },
-        { name: "Container status", value: `${containerStatus}`, inline: true }
+        { name: '\u200B', value: '' },
+        { name: 'Container status', value: `${containerStatus}`, inline: true }
       )
       .setTimestamp()
-      .setFooter({ text: "mDesk by mTech" });
+      .setFooter({ text: 'mDesk by mTech' })
 
     await interaction.editReply({
-      embeds: [dockerStatuseEmbed],
-    });
-  },
-};
+      embeds: [dockerStatuseEmbed]
+    })
+  }
+}
