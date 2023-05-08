b914adc91fc40302d95e87aaa9b497e56638789e
ItzMiracleOwO <s15029@kings.edu.hk>
2023-05-08 20:22:00 +0800

    Some MORE linter fix

--- events/dockerSelect.js
+++ events/dockerSelect.js
@@ -1,56 +1,56 @@
 // Docker command handler
-const { Events, EmbedBuilder } = require("discord.js");
-const config = require("../config.json");
+const { Events, EmbedBuilder } = require('discord.js')
+const config = require('../config.json')
 
-const Docker = require("dockerode");
-const docker = new Docker({ socketPath: config.dockerSock });
+const Docker = require('dockerode')
+const docker = new Docker({ socketPath: config.dockerSock })
 
-const si = require("systeminformation");
+const si = require('systeminformation')
 
 module.exports = {
   name: Events.InteractionCreate,
-  async execute(interaction) {
-    const dockerArray = await si.dockerAll();
-    if (!interaction.isStringSelectMenu()) return;
-    if (interaction.customId !== "containerSelection") return;
+  async execute (interaction) {
+    const dockerArray = await si.dockerAll()
+    if (!interaction.isStringSelectMenu()) return
+    if (interaction.customId !== 'containerSelection') return
 
     await interaction.deferReply({
-      message: "Please wait...",
-      fetchReply: true,
-    });
-    const container = interaction.values[0];
+      message: 'Please wait...',
+      fetchReply: true
+    })
+    const container = interaction.values[0]
 
     const dockerResult = dockerArray.find((element) =>
       element.id.startsWith(container)
-    );
+    )
 
-    const containerHash = dockerResult.id.substring(0, 12);
-    const containerName = dockerResult.name;
-    const containerImage = dockerResult.image;
-    const containerCommand = dockerResult.command;
-    const containerStatus = dockerResult.state;
+    const containerHash = dockerResult.id.substring(0, 12)
+    const containerName = dockerResult.name
+    const containerImage = dockerResult.image
+    const containerCommand = dockerResult.command
+    const containerStatus = dockerResult.state
 
     const dockerStatuseEmbed = new EmbedBuilder()
       .setColor('Random')
-      .setTitle("Docker container status")
+      .setTitle('Docker container status')
       .setDescription(
         `Container Name: ${containerName}` +
-          "\n" +
+          '\n' +
           `Container Hash: ${containerHash}`
       )
       .addFields(
-        { name: "Container image", value: `${containerImage}` + "⠀⠀⠀" },
+        { name: 'Container image', value: `${containerImage}` + '⠀⠀⠀' },
         {
-          name: "Container command",
-          value: `${containerCommand}`,
+          name: 'Container command',
+          value: `${containerCommand}`
         },
-        { name: "Container status", value: `${containerStatus}`, inline: true }
+        { name: 'Container status', value: `${containerStatus}`, inline: true }
       )
       .setTimestamp()
-      .setFooter({ text: "mDesk by mTech" });
+      .setFooter({ text: 'mDesk by mTech' })
 
     await interaction.editReply({
-      embeds: [dockerStatuseEmbed],
-    });
-  },
-};
+      embeds: [dockerStatuseEmbed]
+    })
+  }
+}
