ba069b7316d7fb5004be4da747e98d9f5e3e1857
ItzMiracleOwO <s15029@kings.edu.hk>
2023-05-08 13:59:23 +0800

    Revert "Revert "Linter fix""
    This reverts commit b5177fff0f45d261ce3f0b7526af94aecdf8358f.

---
+++ commands/mdesk/docker.js
@@ -0,0 +1,77 @@
+const {
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+  SlashCommandBuilder,
+  EmbedBuilder,
+} = require("discord.js");
+const config = require("../../config.json");
+
+const Docker = require("dockerode");
+const docker = new Docker({ socketPath: config.dockerSock });
+
+const containerList = [];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("docker")
+    .setDescription("Docker management"),
+  async execute(interaction) {
+    docker.listContainers(async function (err, containers) {
+      if (err) {
+        console.log(err);
+        interaction.reply("An error occured!");
+      } else {
+        containers.map(async function (containerInfo) {
+          // console.log(containerInfo.Id, containerInfo.Names)
+
+          let containers =
+            "Hash: " +
+            containerInfo.Id.substring(0, 12) +
+            " Name: " +
+            containerInfo.Names;
+          containers = containers.replace("/", "");
+          //    containers = `"` + containers + `"`;
+
+          containerList.push(containers);
+          // console.log(containers);
+        });
+        // console.log(containerList);
+        let options = containerList.map((item) => {
+          const [hash, name] = item.split(" Name: ");
+          const hashValue = hash.replace("Hash: ", "");
+          const nameValue = name.trim();
+          const optionData = {
+            emoji: undefined,
+            label: nameValue,
+            description: hashValue,
+            value: hashValue,
+          };
+          console.log(hashValue)
+          return optionData;
+        });
+
+        options = options.slice(0, 23);
+        
+        console.log(options);
+
+        const select = new StringSelectMenuBuilder()
+          .setCustomId("containerSelection")
+          .setPlaceholder("Select container")
+          .addOptions(options);
+
+        const dockerEmbed = new EmbedBuilder()
+          .setColor(0x0099ff)
+          .setTitle("Docker")
+          .setDescription("Select a conainer and input")
+          .setTimestamp()
+          .setFooter({
+            text: "mDesk by mTech",
+          });
+        // console.log(containers);
+        const row = new ActionRowBuilder().addComponents(select);
+
+        interaction.reply({ embeds: [dockerEmbed], components: [row] });
+      }
+    });
+  },
+};
--- events/CommandsInteraction.js
+++ events/CommandsInteraction.js
@@ -1,23 +1,23 @@
 // Command Interaction Handler
-const { Events } = require('discord.js');
+const { Events } = require('discord.js')
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+  name: Events.InteractionCreate,
+  async execute (interaction) {
+    if (!interaction.isChatInputCommand()) return
 
-		const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName)
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`)
+      return
+    }
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
-		}
-	},
-};
+    try {
+      await command.execute(interaction)
+    } catch (error) {
+      console.error(`Error executing ${interaction.commandName}`)
+      console.error(error)
+    }
+  }
+}
