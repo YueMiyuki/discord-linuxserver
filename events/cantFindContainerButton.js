bd17e478dc1ffc53e158a7145e579988511cf578
ItzMiracleOwO <s15029@kings.edu.hk>
2023-05-09 14:40:22 +0800

    God why do I have to add

---
+++ events/cantFindContainerButton.js
@@ -0,0 +1,23 @@
+const { Events, ModalBuilder } = require("discord.js");
+
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== "cantFindContainerButton") return;
+    const modal = new ModalBuilder()
+      .setCustomId("findContainerHashButton")
+      .setTitle("Find Contianer");
+
+    const findContainerHashInput = new TextInputBuilder()
+      .setCustomId("findContainerHashInput")
+      .setLabel("Input your container hash to find it")
+      .setStyle(TextInputStyle.Paragraph);
+
+    const firstActionRow = new ActionRowBuilder().addComponents(
+      findContainerHashInput
+    );
+    modal.addComponents(firstActionRow);
+    await interaction.showModal(modal);
+  },
+};
