90d987a5f3a59a3a48a9f10759895125aaeb0258
ESLint <lint-action@samuelmeuli.com>
2023-05-27 04:23:12 +0000

    使用 Prettier 改進代碼質量

--- commands/mdesk/reload.js
+++ commands/mdesk/reload.js
@@ -1,63 +1,63 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder } = require("discord.js");
 
-const db = require('enhanced.db')
-const userLogin = new db.Table('user')
+const db = require("enhanced.db");
+const userLogin = new db.Table("user");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('reload')
-    .setDescription('Reloads a command.')
+    .setName("reload")
+    .setDescription("Reloads a command.")
     .addStringOption((option) =>
       option
-        .setName('command')
-        .setDescription('The command to reload.')
+        .setName("command")
+        .setDescription("The command to reload.")
         .setRequired(true)
     ),
-  category: 'mdesk',
-  async execute (interaction) {
-    const client = interaction.client
+  category: "mdesk",
+  async execute(interaction) {
+    const client = interaction.client;
     try {
-      const userid = await interaction.member.id
+      const userid = await interaction.member.id;
       if (!userLogin.get(userid)) {
-        return await interaction.reply('You are not logged in!')
+        return await interaction.reply("You are not logged in!");
       }
 
       const commandName = await interaction.options
-        .getString('command', true)
-        .toLowerCase()
-      const command = await interaction.client.commands.get(commandName)
+        .getString("command", true)
+        .toLowerCase();
+      const command = await interaction.client.commands.get(commandName);
 
       if (!command) {
         return await interaction.reply(
           `There is no command with name \`${commandName}\`!`
-        )
+        );
       }
 
       delete require.cache[
         require.resolve(`../${command.category}/${command.data.name}.js`)
-      ]
+      ];
 
       try {
-        await interaction.client.commands.delete(command.data.name)
-        const newCommand = require(`../${command.category}/${command.data.name}.js`)
-        await interaction.client.commands.set(newCommand.data.name, newCommand)
+        await interaction.client.commands.delete(command.data.name);
+        const newCommand = require(`../${command.category}/${command.data.name}.js`);
+        await interaction.client.commands.set(newCommand.data.name, newCommand);
         await interaction.reply(
           `Command \`${newCommand.data.name}\` was reloaded!`
-        )
+        );
       } catch (error) {
-        console.error(error)
+        console.error(error);
         await interaction.reply(
           `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``
-        )
+        );
       }
     } catch (e) {
-      client.log(e, 'error')
+      client.log(e, "error");
       await interaction.editReply({
-        content: 'An error occur!',
+        content: "An error occur!",
         embeds: [],
         components: [],
-        fetchReply: true
-      })
+        fetchReply: true,
+      });
     }
-  }
-}
+  },
+};
--- events/CommandsInteraction.js
+++ events/CommandsInteraction.js
@@ -1,36 +1,39 @@
 // Command Interaction Handler
-const { Events } = require('discord.js')
+const { Events } = require("discord.js");
 
 module.exports = {
   name: Events.InteractionCreate,
-  async execute (interaction) {
-    const client = interaction.client
+  async execute(interaction) {
+    const client = interaction.client;
     try {
-      if (!interaction.isChatInputCommand()) return
+      if (!interaction.isChatInputCommand()) return;
 
-      const command = interaction.client.commands.get(interaction.commandName)
+      const command = interaction.client.commands.get(interaction.commandName);
 
       if (!command) {
         client.log(
-          `No command matching ${interaction.commandName} was found.`, 'error'
-        )
-        return
+          `No command matching ${interaction.commandName} was found.`,
+          "error"
+        );
+        return;
       }
 
       try {
-        await command.execute(interaction)
-        client.log(`Command ${command} ran by ${interaction.user.tag} (${interaction.member.id})`)
+        await command.execute(interaction);
+        client.log(
+          `Command ${command} ran by ${interaction.user.tag} (${interaction.member.id})`
+        );
       } catch (error) {
-        client.log(error, 'error')
+        client.log(error, "error");
       }
     } catch (e) {
-      client.log(e, 'error')
+      client.log(e, "error");
       await interaction.editReply({
-        content: 'An error occur!',
+        content: "An error occur!",
         embeds: [],
         components: [],
-        fetchReply: true
-      })
+        fetchReply: true,
+      });
     }
-  }
-}
+  },
+};
