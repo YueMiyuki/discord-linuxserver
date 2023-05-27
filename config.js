e79d80b39a754eb8038d10a3407acb8831caf470
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2023-05-27 12:24:50 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clientId: process.env.clientId || "",
+  token:
+    process.env.token ||
+    "",
+  address: process.env.address || "",
+  botUser: process.env.botUser || "",
+  dockerSock: process.env.dockerSock || "",
+};
