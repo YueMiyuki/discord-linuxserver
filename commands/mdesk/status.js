ee6e230aa9c536747e7a1bbc89f7fd7274e6a587
ESLint <lint-action@samuelmeuli.com>
2023-05-27 04:23:09 +0000

    使用 ESLint 改進代碼質量

--- commands/mdesk/status.js
+++ commands/mdesk/status.js
@@ -1,96 +1,96 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const si = require("systeminformation");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const si = require('systeminformation')
 
-async function getData() {
+async function getData () {
   try {
     const getObject = {
-      cpu: "manufacturer, brand, speed",
-      osInfo: "platform, release",
-      mem: "total, free, used, cached",
-      dockerInfo: "containers, containersRunning",
-      currentLoad: "avgLoad, currentLoad",
-    };
+      cpu: 'manufacturer, brand, speed',
+      osInfo: 'platform, release',
+      mem: 'total, free, used, cached',
+      dockerInfo: 'containers, containersRunning',
+      currentLoad: 'avgLoad, currentLoad'
+    }
 
-    const data = await si.get(getObject);
-    return data;
+    const data = await si.get(getObject)
+    return data
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("status")
-    .setDescription("Show server status"),
-  category: "mdesk",
+    .setName('status')
+    .setDescription('Show server status'),
+  category: 'mdesk',
 
-  async execute(interaction) {
-    const client = interaction.client;
+  async execute (interaction) {
+    const client = interaction.client
     try {
       await interaction.deferReply({
-        content: "Please wait...",
-        fetchReply: true,
-      });
+        content: 'Please wait...',
+        fetchReply: true
+      })
       getData().then(async (d) => {
-        const CLoad = Math.round(d.currentLoad.currentLoad);
-        const ALoad = Math.round(d.currentLoad.avgLoad);
+        const CLoad = Math.round(d.currentLoad.currentLoad)
+        const ALoad = Math.round(d.currentLoad.avgLoad)
 
         const StatusEmbed = new EmbedBuilder()
-          .setColor("Random")
-          .setTitle("mDesk Status")
+          .setColor('Random')
+          .setTitle('mDesk Status')
           .setDescription(`Websocket heartbeat: ${client.ws.ping}ms`)
           .addFields(
             {
-              name: "CPU",
+              name: 'CPU',
               value:
                 `Manufacturer: ${d.cpu.manufacturer}\n` +
                 `Brand: ${d.cpu.brand}\n` +
                 `Frequency: ${d.cpu.speed}` +
                 `Average Load: ${ALoad}` +
-                " " +
+                ' ' +
                 `Current Load: ${CLoad}`,
-              inline: true,
+              inline: true
             },
             {
-              name: "Memory",
+              name: 'Memory',
               value:
                 `Total: ${d.mem.total}\n` +
                 `Used: ${d.mem.used}\n` +
                 `Free: ${d.mem.free}\n` +
                 `Cached: ${d.mem.cached}`,
-              inline: true,
+              inline: true
             },
-            { name: "\u200B", value: "\u200B" },
+            { name: '\u200B', value: '\u200B' },
             {
-              name: "OS",
+              name: 'OS',
               value:
                 `Platform: ${d.osInfo.platform}\n` +
                 `Release: ${d.osInfo.release}`,
-              inline: true,
+              inline: true
             },
             {
-              name: "Docker",
+              name: 'Docker',
               value:
                 `Containers: ${d.dockerInfo.containers}\n` +
                 `Containers Running: ${d.dockerInfo.containersRunning}`,
-              inline: true,
+              inline: true
             }
           )
           .setTimestamp()
           .setFooter({
-            text: "mDesk by mTech",
-          });
+            text: 'mDesk by mTech'
+          })
 
-        await interaction.editReply({ embeds: [StatusEmbed] });
-      });
+        await interaction.editReply({ embeds: [StatusEmbed] })
+      })
     } catch (e) {
-      client.log(e, "error");
+      client.log(e, 'error')
       await interaction.editReply({
-        content: "An error occur!",
+        content: 'An error occur!',
         embeds: [],
         components: [],
-        fetchReply: true,
-      });
+        fetchReply: true
+      })
     }
-  },
-};
+  }
+}
