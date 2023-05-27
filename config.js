04ab5853284a6e90767b6039d8449fedf511d173
ESLint <lint-action@samuelmeuli.com>
2023-05-27 04:25:25 +0000

    使用 ESLint 改進代碼質量

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  clientId: process.env.clientId || "",
+  clientId: process.env.clientId || '',
   token:
     process.env.token ||
-    "",
-  address: process.env.address || "",
-  botUser: process.env.botUser || "",
-  dockerSock: process.env.dockerSock || "",
-};
+    '',
+  address: process.env.address || '',
+  botUser: process.env.botUser || '',
+  dockerSock: process.env.dockerSock || ''
+}
