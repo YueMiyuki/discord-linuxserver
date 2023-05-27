aa2789b5d28b7c7f19afa390c13d63bf22bb8996
ESLint <lint-action@samuelmeuli.com>
2023-05-27 04:40:41 +0000

    使用 Prettier 改進代碼質量

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  clientId: process.env.clientId || '1102946569579397221',
+  clientId: process.env.clientId || "1102946569579397221",
   token:
     process.env.token ||
-    'MTEwMjk0NjU2OTU3OTM5NzIyMQ.GfBdNy.VenSWgZCR-gOrpdaDf8gkkB5xe8UE1riFiLxQs',
-  address: process.env.address || '192.168.1.144',
-  botUser: process.env.botUser || 'mdeskBot',
-  dockerSock: process.env.dockerSock || '/var/run/docker.sock'
-}
+    "MTEwMjk0NjU2OTU3OTM5NzIyMQ.GfBdNy.VenSWgZCR-gOrpdaDf8gkkB5xe8UE1riFiLxQs",
+  address: process.env.address || "192.168.1.144",
+  botUser: process.env.botUser || "mdeskBot",
+  dockerSock: process.env.dockerSock || "/var/run/docker.sock",
+};
