bffc746a0f011c980272ae1dc275f834877dbc97
ESLint <lint-action@samuelmeuli.com>
2023-05-27 04:51:16 +0000

    使用 ESLint 改進代碼質量

--- commands/mdesk/login.js
+++ commands/mdesk/login.js
@@ -3,61 +3,61 @@ const {
   ModalBuilder,
   TextInputBuilder,
   TextInputStyle,
-  SlashCommandBuilder,
-} = require("discord.js");
+  SlashCommandBuilder
+} = require('discord.js')
 
-const db = require("enhanced.db");
-const userLogin = new db.Table("user");
+const db = require('enhanced.db')
+const userLogin = new db.Table('user')
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("login")
-    .setDescription("Login to mDesk"),
-  category: "mdesk",
+    .setName('login')
+    .setDescription('Login to mDesk'),
+  category: 'mdesk',
 
-  async execute(interaction) {
-    const client = interaction.client;
+  async execute (interaction) {
+    const client = interaction.client
     try {
-      const userid = await interaction.member.id;
+      const userid = await interaction.member.id
 
       if (userLogin.get(userid)) {
-        return await interaction.reply("You are already logged in!");
+        return await interaction.reply('You are already logged in!')
       }
 
       const modal = new ModalBuilder()
-        .setCustomId("mdesklogin")
-        .setTitle("Login to mDesk");
+        .setCustomId('mdesklogin')
+        .setTitle('Login to mDesk')
 
       const usernameInput = new TextInputBuilder()
         .setRequired(true)
-        .setCustomId("username")
-        .setLabel("mDesk login username:")
-        .setPlaceholder("Username")
-        .setStyle(TextInputStyle.Short);
+        .setCustomId('username')
+        .setLabel('mDesk login username:')
+        .setPlaceholder('Username')
+        .setStyle(TextInputStyle.Short)
 
       const pwdInput = new TextInputBuilder()
         .setRequired(true)
-        .setCustomId("pwd")
-        .setLabel("mDesk login password:")
-        .setPlaceholder("Password")
-        .setStyle(TextInputStyle.Short);
+        .setCustomId('pwd')
+        .setLabel('mDesk login password:')
+        .setPlaceholder('Password')
+        .setStyle(TextInputStyle.Short)
 
       const firstActionRow = new ActionRowBuilder().addComponents(
         usernameInput
-      );
-      const secondActionRow = new ActionRowBuilder().addComponents(pwdInput);
+      )
+      const secondActionRow = new ActionRowBuilder().addComponents(pwdInput)
 
-      modal.addComponents(firstActionRow, secondActionRow);
+      modal.addComponents(firstActionRow, secondActionRow)
 
-      await interaction.showModal(modal);
+      await interaction.showModal(modal)
     } catch (e) {
-      client.log(e, "error");
+      client.log(e, 'error')
       await interaction.editReply({
-        content: "An error occur!",
+        content: 'An error occur!',
         embeds: [],
         components: [],
-        fetchReply: true,
-      });
+        fetchReply: true
+      })
     }
-  },
-};
+  }
+}
