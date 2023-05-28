fa3bf4ead313221754e023dca1176d711f29a28f
ItzMiracleOwO <s15029@kings.edu.hk>
2023-05-28 11:49:24 +0800

    Yeah yeah yeah

--- base/deploy.js
+++ base/deploy.js
@@ -1,40 +1,35 @@
-const { REST, Routes } = require("discord.js");
-const fs = require("node:fs");
-const path = require("node:path");
+const { REST, Routes } = require('discord.js')
+const fs = require('node:fs')
+const path = require('node:path')
 
-try {
-  // Config
-  config = require("../config.js");
-  log = require("./log.js").log;
-} catch (e) {
-  console.log(e);
-}
+const config = require('../config.js')
+const log = require('./log.js').log
 
-const clientId = config.clientId;
-const token = config.token;
+const clientId = config.clientId
+const token = config.token
 
-const commands = [];
+const commands = []
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, "../", "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath = path.join(__dirname, '../', 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
   // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
+  const commandsPath = path.join(foldersPath, folder)
   const commandFiles = fs
     .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+    .filter((file) => file.endsWith('.js'))
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON())
     } else {
       log(
         `The command at ${filePath} is missing a required "data" or "execute" property.`,
-        "warn"
-      );
+        'warn'
+      )
     }
   }
 }
@@ -47,20 +42,20 @@ const rest = new REST().setToken(token);
   try {
     log(
       `Started refreshing ${commands.length} application (/) commands.`,
-      "info"
-    );
+      'info'
+    )
 
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(Routes.applicationCommands(clientId), {
-      body: commands,
-    });
+      body: commands
+    })
 
     log(
-      `Successfully reloaded ${data.length} application (/) commands.` + "\n",
-      "info"
-    );
+      `Successfully reloaded ${data.length} application (/) commands.` + '\n',
+      'info'
+    )
   } catch (error) {
     // And of course, make sure you catch and log any errors!
-    console.error(error);
+    console.error(error)
   }
-})();
+})()
--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  clientId: process.env.clientId || "1111877507847823431",
-  token:
-    process.env.token ||
-    "MTExMTg3NzUwNzg0NzgyMzQzMQ.GU3-wo.tABfEMZsX6_9DPWGYp-2rpF8XeZqEMp4md5EaI",
-  address: process.env.address || "192.168.1.144",
-  botUser: process.env.botUser || "mdeskBot",
-  dockerSock: process.env.dockerSock || "/var/run/docker.sock",
-};
