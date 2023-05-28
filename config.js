e70f45dae20f0cfe0332c731094b42431a755c77
ItzMiracleOwO <s15029@kings.edu.hk>
2023-05-28 11:43:25 +0800

    why my .gitignore file is not working

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  clientId: process.env.clientId || "",
-  token: process.env.token || "",
-  address: process.env.address || "",
-  botUser: process.env.botUser || "",
-  dockerSock: process.env.dockerSock || "",
+  clientId: process.env.clientId || "1111877507847823431",
+  token:
+    process.env.token ||
+    "MTExMTg3NzUwNzg0NzgyMzQzMQ.GU3-wo.tABfEMZsX6_9DPWGYp-2rpF8XeZqEMp4md5EaI",
+  address: process.env.address || "192.168.1.144",
+  botUser: process.env.botUser || "mdeskBot",
+  dockerSock: process.env.dockerSock || "/var/run/docker.sock",
 };
