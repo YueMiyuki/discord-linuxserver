9b4b9239b6eabf2fae1cc3a24f20b5b87cc70148
ESLint <lint-action@samuelmeuli.com>
2023-05-30 02:46:28 +0000

    使用 Prettier 改進代碼質量

--- base/runCi.js
+++ base/runCi.js
@@ -1,33 +1,33 @@
 module.exports = {
   ci: async function () {
-    const { execSync } = require('child_process')
-    const log = require('./log.js').log
+    const { execSync } = require("child_process");
+    const log = require("./log.js").log;
 
-    log('Startup test succeed!', 'ci')
-    log('Node version: ' + process.version + '\n', 'debug')
+    log("Startup test succeed!", "ci");
+    log("Node version: " + process.version + "\n", "debug");
 
-    log('Attempting to run deploy...', 'ci')
+    log("Attempting to run deploy...", "ci");
 
     try {
-      process.stdout.write(execSync('node ./base/deploy.js'))
+      process.stdout.write(execSync("node ./base/deploy.js"));
     } catch (e) {
-      log('Deploy script return failed!', 'error')
-      throw new Error(e)
+      log("Deploy script return failed!", "error");
+      throw new Error(e);
     }
 
-    log('Attempting to run systeminformation methods test...', 'ci')
+    log("Attempting to run systeminformation methods test...", "ci");
 
     try {
       process.stdout.write(
-        execSync('node ./base/systeminformationMethodTest.js')
-      )
+        execSync("node ./base/systeminformationMethodTest.js")
+      );
     } catch (e) {
-      log('Deploy script return failed!', 'error')
-      throw new Error(e)
+      log("Deploy script return failed!", "error");
+      throw new Error(e);
     }
 
-    log('Test finished!', 'info')
-    log('Killing process...', 'ci')
-    process.exit()
-  }
-}
+    log("Test finished!", "info");
+    log("Killing process...", "ci");
+    process.exit();
+  },
+};
