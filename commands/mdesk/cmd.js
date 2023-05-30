391d870218f0415f303a7ca815d3b7efaeb6c2be
ESLint <lint-action@samuelmeuli.com>
2023-05-30 02:47:50 +0000

    使用 Prettier 改進代碼質量

--- commands/mdesk/cmd.js
+++ commands/mdesk/cmd.js
@@ -3,47 +3,47 @@ const {
   SlashCommandBuilder,
   ModalBuilder,
   TextInputBuilder,
-  TextInputStyle
-} = require('discord.js')
+  TextInputStyle,
+} = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('cmd').setDescription('Run command'),
-  category: 'mdesk',
+  data: new SlashCommandBuilder().setName("cmd").setDescription("Run command"),
+  category: "mdesk",
 
-  async execute (interaction) {
-    const client = interaction.client
+  async execute(interaction) {
+    const client = interaction.client;
     try {
-      const userid = await interaction.member.id
+      const userid = await interaction.member.id;
 
-      const auth = await client.dbAuth(userid)
+      const auth = await client.dbAuth(userid);
       if (!auth) {
-        await interaction.reply('You are not authorized to use this command!')
-        return
+        await interaction.reply("You are not authorized to use this command!");
+        return;
       }
 
       const modal = new ModalBuilder()
-        .setCustomId('commandInput')
-        .setTitle('Run command')
+        .setCustomId("commandInput")
+        .setTitle("Run command");
 
       const commandInput = new TextInputBuilder()
         .setRequired(true)
-        .setCustomId('command')
-        .setPlaceholder('whoami')
-        .setLabel('What command do you want to run?')
-        .setStyle(TextInputStyle.Short)
+        .setCustomId("command")
+        .setPlaceholder("whoami")
+        .setLabel("What command do you want to run?")
+        .setStyle(TextInputStyle.Short);
 
-      const firstActionRow = new ActionRowBuilder().addComponents(commandInput)
-      modal.addComponents(firstActionRow)
+      const firstActionRow = new ActionRowBuilder().addComponents(commandInput);
+      modal.addComponents(firstActionRow);
 
-      await interaction.showModal(modal)
+      await interaction.showModal(modal);
     } catch (e) {
-      client.log(e, 'error')
+      client.log(e, "error");
       await interaction.editReply({
-        content: 'An error occur!',
+        content: "An error occur!",
         embeds: [],
         components: [],
-        fetchReply: true
-      })
+        fetchReply: true,
+      });
     }
-  }
-}
+  },
+};
