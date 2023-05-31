f767ef43e24d145c1f0b832b8b5a9cc992537ca1
ESLint <lint-action@samuelmeuli.com>
2023-05-31 03:52:37 +0000

    使用 Prettier 改進代碼質量

--- base/dbAuth.js
+++ base/dbAuth.js
@@ -1,12 +1,12 @@
 module.exports = {
   dbAuth: async function (userid) {
-    const db = require('enhanced.db')
-    const userLogin = new db.Table('user')
+    const db = require("enhanced.db");
+    const userLogin = new db.Table("user");
 
     if (!userLogin.get(userid)) {
-      return false
+      return false;
     } else {
-      return true
+      return true;
     }
-  }
-}
+  },
+};
